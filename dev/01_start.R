567fa7c762b8fadd0fcf9ab9fc5459876aa36b9b
haighd <djhaight@gmail.com>
2021-10-31 10:03:49 -0400

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -25,7 +25,7 @@ golem::fill_desc(
   author_first_name = "Dan", # Your First Name
   author_last_name = "Haight", # Your Last Name
   author_email = "djhaight@gmail.com", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional) 
+  repo_url = "https://github.com/haighd/portfolioopt" # The URL of the GitHub Repo (optional) 
 )     
 
 ## Set {golem} options ----
